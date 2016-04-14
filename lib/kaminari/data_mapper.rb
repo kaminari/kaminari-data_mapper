24e4f353598c122fbd30e496224278fdba5715d1
Akira Matsuda <ronnie@dio.jp>
2016-04-14 17:52:27 +0900

    Copy DataMapper initialization from kaminari/hooks.rb

--- lib/kaminari/data_mapper.rb
+++ lib/kaminari/data_mapper.rb
@@ -2,6 +2,14 @@ require "kaminari/data_mapper/version"
 
 module Kaminari
   module DataMapper
-    # Your code goes here...
   end
+end
+
+begin; require 'data_mapper'; rescue LoadError; end
+if defined? ::DataMapper
+  require 'dm-aggregates'
+  require 'kaminari/models/data_mapper_extension'
+  ::DataMapper::Collection.send :include, Kaminari::DataMapperExtension::Collection
+  ::DataMapper::Model.append_extensions Kaminari::DataMapperExtension::Model
+  # ::DataMapper::Model.send :extend, Kaminari::DataMapperExtension::Model
 end
