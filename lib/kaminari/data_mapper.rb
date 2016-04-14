25922648c638ce49363c1da7ee4d8d5e2d273c17
Akira Matsuda <ronnie@dio.jp>
2016-04-14 18:02:43 +0900

    Explicitly require kaminari and data_mapper

--- lib/kaminari/data_mapper.rb
+++ lib/kaminari/data_mapper.rb
@@ -1,15 +1,14 @@
 require "kaminari/data_mapper/version"
+require 'kaminari'
+require 'data_mapper'
 
 module Kaminari
   module DataMapper
   end
 end
 
-begin; require 'data_mapper'; rescue LoadError; end
-if defined? ::DataMapper
-  require 'dm-aggregates'
-  require 'kaminari/data_mapper/data_mapper_extension'
-  ::DataMapper::Collection.send :include, Kaminari::DataMapperExtension::Collection
-  ::DataMapper::Model.append_extensions Kaminari::DataMapperExtension::Model
-  # ::DataMapper::Model.send :extend, Kaminari::DataMapperExtension::Model
-end
+require 'dm-aggregates'
+require 'kaminari/data_mapper/data_mapper_extension'
+::DataMapper::Collection.send :include, Kaminari::DataMapperExtension::Collection
+::DataMapper::Model.append_extensions Kaminari::DataMapperExtension::Model
+# ::DataMapper::Model.send :extend, Kaminari::DataMapperExtension::Model
